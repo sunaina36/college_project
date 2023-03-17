395f08533ed9c8d3b58a0b5400e6170e810b1512
JaiShankar <jaishankar.a1603@gmail.com>
2023-03-17 13:11:22 +0530

    corrected few errors and made webpage more responsive

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
-let emailInput = document.querySelector(".emailInput");
-let phoneInput = document.querySelector(".changeToPhoneNumber");
-let changePreferenceToPhone = document.querySelector(".loginWithPhoneNumber");
-let changePreferenceToEmail = document.querySelector(".loginWithEmail");
+let emailInput = document.querySelector(".emailInput");// email input
+let phoneInput = document.querySelector(".changeToPhoneNumber");// phone input
+let changePreferenceToPhone = document.querySelector(".loginWithPhoneNumber"); // link for login with phone number
+let changePreferenceToEmail = document.querySelector(".loginWithEmail");// link for login with email
+let logInPassword = document.querySelector(".loginPassword");// password input
+let showPassword = document.querySelector(".passwordShow");// show eye icon
+let hidePassword = document.querySelector(".passwordhide");// hide eye icon
+
+// function to change preference of login
 function changeLoginPreference(){
     console.log("j")
     if(phoneInput.style.display == "none"|| phoneInput.style.display== ""){
-        console.log("jai Shankar")
+        // console.log("jai Shankar")
         emailInput.style.display = "none";
         changePreferenceToPhone.style.display = "none";
         phoneInput.style.display = "block";
+        // phoneInput.setAttribute("required","");
+        // emailInput.required = flase;
         changePreferenceToEmail.style.display = "block";
     }
     else{
         console.log("jai")
         emailInput.style.display = "block";
+        // phoneInput.required = false;
+        // emailInput.required = true;
         changePreferenceToPhone.style.display = "block";
         phoneInput.style.display = "none";
         changePreferenceToEmail.style.display = "none";
     }
 }
-let logInPassword = document.querySelector(".loginPassword");
-let showPassword = document.querySelector(".passwordShow");
-let hidePassword = document.querySelector(".passwordhide");
+
+// function to show and hide the password
 function changePasswordVisibility(){
     if(hidePassword.style.display == "none"|| hidePassword.style.display == ""){
         showPassword.style.display = "none";
@@ -35,4 +43,8 @@ function changePasswordVisibility(){
         hidePassword.style.display = "none";
 
     }
+}
+
+function emailValidation(){
+    
 }
