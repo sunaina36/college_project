f3c92eb8608049f3b2af49640fa51a06e910b3f3
Sunaina <sunainab36@gmail.com>
2023-05-15 23:37:55 +0530

    changes made in transaction page

--- src/controller/Email.js
+++ src/controller/Email.js
@@ -41,7 +41,20 @@ let sendUserDetail= function(us){
     from: "jaishankar.a1603@gmail.com",
     to: us.email,
     subject: "Thank You For choosing Pay4U",
-    text:`welcome ${us.firstName}` 
+    text:`welcome ${us.firstName},
+    Thank you for choosing Pay4U Bank for 
+    banking needs.
+
+    Your Account number is 
+    
+    Login with your registered Email or Phone to access your account.
+
+    Enjoy instant Banking anytime with Pay4U banking website.
+    
+    Regards,
+    
+    PayForU Team
+    ` 
   };
   transporter.sendMail(mailOption, function(error, info){
     if (error) {
@@ -57,7 +70,17 @@ let sendtransferDetail = function(us){
     from: "jaishankar.a1603@gmail.com",
     to: us.email,
     subject: "Thank You For choosing Pay4U",
-    text:`welcome ${us.firstName}` 
+    text:`welcome ${us.firstName}
+    Thank you for banking with Pay4U Bank.
+    
+    Your account number ***5319 is debited by INR 650.00 on 13-05-2023 02:02:29 PM IST on account of outward fund transfer.
+
+    For clarification please contact Pay4U Customer Care  +91-9999999999,  or email jaishankar.a1603@gmail.com
+
+    Regards,
+
+    Pay4U Team
+    ` 
   };
   transporter.sendMail(mailOption, function(error, info){
     if (error) {
