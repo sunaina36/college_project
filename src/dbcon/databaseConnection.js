61661fe75d1712f9302ee9fe84973dfab3a12d92
JaiShankar <jaishankar.a1603@gmail.com>
2023-08-16 11:53:51 +0530

    completed online banking website

--- src/dbcon/databaseConnection.js
+++ src/dbcon/databaseConnection.js
@@ -3,7 +3,7 @@ main().catch(err => console.log(err));
 
 async function main() {
 
-await mongoose.connect('mongodb+srv://jaishankar:bDg8Qt7TSHtK152g@cluster0.58q9irm.mongodb.net/?retryWrites=true&w=majority');
+  await mongoose.connect('mongodb+srv://jai:VocEC5VZ4PgdNN8r@cluster0.kqhxi14.mongodb.net/?retryWrites=true&w=majority');
   console.log("database connected");
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
